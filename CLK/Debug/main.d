8ec0b60f2b752c30405cd884caf974190e428dc1
Julien Taubert <Julien.Taubert@stud.htwk-leipzig.de>
2024-11-16 20:08:11 +0100

    Put everything in a Microchip Studio Project

---
+++ CLK/Debug/main.d
@@ -0,0 +1,55 @@
+main.d main.o: .././main.c \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ E:\Programme\AVR\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom48a.h \
+ E:\Programme\AVR\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom48.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sleep.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\power.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+E:\Programme\AVR\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom48a.h:
+
+E:\Programme\AVR\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom48.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sleep.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\power.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+e:\programme\avr\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
